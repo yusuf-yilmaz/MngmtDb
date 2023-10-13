d5c66b2d8759660460ccf5858f6e59ef01f64e28
yusuf-yilmaz <70882832+yusuf-yilmaz@users.noreply.github.com>
2023-10-13 16:06:36 +0200

    Add files via upload

---
+++ vendor/yusuf/metriccollector/src/Collectors/metricscollector.php
@@ -0,0 +1,15 @@
+<?php
+namespace yusuf\metriccollector\Collectors;
+
+class MetricsCollector
+{
+    public function collectGlobalMetrics()
+    {
+
+    }
+
+    public function collectCustomMetric($key, $callback)
+    {
+
+    }
+}
